decb4fb939651d7604385caa4788e92ddf92cdaf
Matt Strayer <mattstrayer77@gmail.com>
2022-02-21 22:29:01 -0700

    initial commit

---
+++ src/Display.js
@@ -0,0 +1,6 @@
+export class Display {
+    constructor() {
+        console.log('Created a new display')
+        this.screen = document.querySelector('canvas')
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { Chip8 } from "./Chip8";
+
+//creating new instance of Chip8 class
+const chip8 = new Chip8();
